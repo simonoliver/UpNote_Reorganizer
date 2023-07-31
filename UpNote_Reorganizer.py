aaf68c4de6e0abb7408d21e8a00247d067496119
Simon Oliver <simon@handcircus.com>
2023-07-31 15:07:38 +0200

    Move images, remove category metadata

--- UpNote_Reorganizer.py
+++ UpNote_Reorganizer.py
@@ -1,8 +1,12 @@
 import os
 import shutil
 import frontmatter #pip install python-frontmatter
+import re
 
 baseDir = "Notes"
+## Pattern is  (Files/XXX)
+regex = r"(?:[!]\[(?P<caption>.*?)\])\((?P<image>.*?)(?P<description>\".*?\")?\)"
+
 if not os.path.isdir(baseDir):
     os.mkdir(baseDir)
 
@@ -12,6 +16,9 @@ for file in os.listdir(os.getcwd()):
 
         note = frontmatter.load(file)
 
+        ## Copy any images
+        post_content=note.content
+       
         if 'categories' in note:
             for c in note['categories']:
 
@@ -21,7 +28,31 @@ for file in os.listdir(os.getcwd()):
 
                 if not os.path.isdir(dest):
                     os.makedirs(dest)
-                    
-                shutil.copy(file, dest)
+        
+                ## shutil.copy(file, dest)
         else:
-            shutil.copy(file, baseDir)
+            dest=baseDir   
+            ## shutil.copy(file, baseDir)
+
+        print("Processing note "+file)
+        # Find all file matches
+        matches = re.finditer(regex, post_content, re.MULTILINE)
+        for matchNum, match in enumerate(matches, start=1):
+            image_path = match.group(2)
+            # Only local refs
+            if "Files" in image_path:
+                from urllib.parse import unquote
+                converted_path=unquote(image_path)
+                print("Found image "+converted_path+" in file "+ file+"  moving to "+dest)
+            
+                # Copy to destination
+                shutil.copy(converted_path, dest)
+            
+        # Remove file path ref
+        post_content=post_content.replace("Files/","")
+
+
+        fullpath= os.path.join(dest, file)
+        outfile = open(fullpath, "w", encoding="utf-8")
+        outfile.write(post_content)
+    
